c5a36de9151f6733b6754c8d4838960aa99fa281
Bekzod Valiev <bekzodvaliev@Bekzods-MacBook-Pro.local>
2023-12-05 10:23:47 +0500

    added new files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import Home from "./pages/home/Home"
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login"
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login/>}/>
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export default App
