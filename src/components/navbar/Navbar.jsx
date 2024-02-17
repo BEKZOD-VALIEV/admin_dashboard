0380e603435f03ac6776459d85d093feaf13d0fb
Bekzod Valiev <bekzodvaliev@Bekzods-MacBook-Pro.local>
2024-02-17 17:59:54 +0500

    addedcomponents

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,44 +1,52 @@
-import "./navbar.scss"
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined"
-import LanguageIcon from '@mui/icons-material/Language';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import ListIcon from '@mui/icons-material/List';
+import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageIcon from "@mui/icons-material/Language";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import FullscreenExitIcon from "@mui/icons-material/FullscreenExit";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import ListIcon from "@mui/icons-material/List";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="wrapper">
         <div className="search">
           <input type="text" placeholder="Search..." />
           <SearchOutlinedIcon />
-          
         </div>
         <div className="items">
           <div className="item">
-            <LanguageIcon className="icon"/>
+            <LanguageIcon className="icon" />
           </div>
           <div className="item">
-            <DarkModeIcon className="icon"/>
+            <DarkModeIcon className="icon" />
           </div>
           <div className="item">
-            <FullscreenExitIcon className="icon"/>
+            <FullscreenExitIcon className="icon" />
           </div>
           <div className="item">
-            <NotificationsNoneIcon className="icon"/>
+            <NotificationsNoneIcon className="icon" />
+            <div className="counter">1</div>
           </div>
           <div className="item">
-            <ChatBubbleOutlineIcon className="icon"/>
+            <ChatBubbleOutlineIcon className="icon" />
+            <div className="counter">2</div>
           </div>
           <div className="item">
-            <ListIcon className="icon"/>
+            <ListIcon className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/839586/pexels-photo-839586.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className="avatar"
+            />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
